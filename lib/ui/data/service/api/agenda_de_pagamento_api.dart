c086ae107f938017b2af1ac009e1dece3fac3753
MatheusHard <matheus@novetech.com.br>
2026-02-25 08:53:15 -0300

    [REFACTOR] clean code apis

--- lib/ui/data/service/api/agenda_de_pagamento_api.dart
+++ lib/ui/data/service/api/agenda_de_pagamento_api.dart
@@ -18,7 +18,7 @@ class AgendaDePagamentoApi  {
   AgendaDePagamentoApi(BuildContext context) {
     _context = context;
   }
-
+  //Add
   Future<AgendaDePagamento?> addAgendaDePagamento(AgendaDePagamentoDTO agenda) async {
     var response = await _customDio.dio.post(URL,
       data: agenda.toJson(),
@@ -26,84 +26,38 @@ class AgendaDePagamentoApi  {
     );
     return response.data != null ? AgendaDePagamento.fromJson(response.data) : null;
   }
-
+  //Find All
   Future<List<AgendaDePagamento>> getList() async {
     var response = await _customDio.dio.get(URL,
       options: Options(
       headers: await Utils.requestToken()),);
-
     if (response.statusCode == 200) {
-      var lista = response.data;
-      // Aqui você pode fazer o mapeamento de lista para objetos Cliente
-      List<AgendaDePagamento> agendasdepagamento = (lista as List)
-          .map((json) => AgendaDePagamento.fromJson(json))
-          .toList();
-
-      return agendasdepagamento;
+      return (response.data as List).map((json) => AgendaDePagamento.fromJson(json)).toList();
     }
-
     return [];
   }
-
+  //Find All By Filters
   Future<List<AgendaDePagamento>> getListByFilter(AgendaDePagamentoDTO filtros) async {
     var response = await _customDio.dio.post(
       URL+FILTRAR,
       data: 	filtros.toJson(),
-      options: Options(
-        headers: await Utils.requestToken()),);
+      options: Options(headers: await Utils.requestToken()),);
     if (response.statusCode == 200) {
-      var lista = response.data;
-      // Aqui você pode fazer o mapeamento de lista para objetos Agendamento
-      List<AgendaDePagamento> faturas = (lista as List)
-          .map((json) => AgendaDePagamento.fromJson(json))
-          .toList();
-
-      return faturas;
+      return (response.data as List).map((json) => AgendaDePagamento.fromJson(json)).toList();
     }
     return [];
-
   }
-
+  //Find Object
   Future<AgendaDePagamento?> getOneByFilter(AgendaDePagamentoDTO filtros) async {
     var response = await _customDio.dio.post(
       URL+FIND_ONE,
       data: 	filtros.toJson(),
-      options: Options( headers: await Utils.requestToken()),);
+      options: Options(headers: await Utils.requestToken()),);
     if (response.statusCode == 200) {
-      var resposta = response.data;
-      AgendaDePagamento fatura =  AgendaDePagamento.fromJson(resposta);
-      return fatura;
+      return AgendaDePagamento.fromJson(response.data);
     }
     return null;
   }
 }
-  /*@override
-  Future<bool> updateCliente(Cliente cliente, int user_id) async {
-    var token = await Utils.recuperarToken(); // Pegue do localStorage, SharedPreferences, etc.
 
-    var response = await _customDio.dio.put(URL,
-      data: {
-        "id": cliente.id,
-        "name": cliente.name,
-        "createdAt": cliente.createdAt,
-        "updatedAt": cliente.updatedAt,
-        "name": cliente.name,
-        "cpf": "",
-        "email": cliente.email,
-        "telephone": cliente.telephone,
-        "deletado": cliente.deletado,
-        "user": {
-          "id":  user_id
-        },
-        "photoName": cliente.photoName,
-        "imagemBase64": cliente.imagemBase64
-      },
-      options: Options(
-        headers: {
-          'Authorization': 'Bearer $token',
-        },),
-    );
-    return response.statusCode == 200;
-  }
-*/
 
