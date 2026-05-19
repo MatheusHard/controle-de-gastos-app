765c85c3e3a0f3e0d598b8aa051558d84a78bfbd
MatheusHard <matheus@novetech.com.br>
2026-05-19 09:55:44 -0300

    WIP -  baixar_relatorio_gastos quase completo

--- lib/ui/data/service/api/relatorio_api.dart
+++ lib/ui/data/service/api/relatorio_api.dart
@@ -1,15 +1,15 @@
 import 'dart:io';
 
 import 'package:controle_de_gastos_app/ui/core/configs/dio/configs.dart';
+import 'package:controle_de_gastos_app/ui/core/constants/enums/app_platform.dart';
 import 'package:controle_de_gastos_app/ui/data/dtos/gasto_dto.dart';
-import 'package:controle_de_gastos_app/ui/data/model/login.dart';
-import 'package:controle_de_gastos_app/ui/data/model/user.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:open_filex/open_filex.dart';
-import 'package:path_provider/path_provider.dart';
-
-import '../../../core/constants/routes/app_routes.dart';
+import 'package:web/web.dart' as web;
+import 'dart:js_interop';
+import 'dart:typed_data';
+import 'package:universal_html/html.dart' as html;
 import '../../../core/utils/utils.dart';
 
 class RelatorioApi {
@@ -21,36 +21,52 @@ class RelatorioApi {
   }
 
   Future<void> getRelatorioGastos(GastoDTO filtros) async {
-
     final configs = await Configs.create();
     try {
-
       final response = await configs.dio.post(
         "/relatorio/gastos",
         data: filtros.toJson(),
-
         options: Options(
-
           responseType: ResponseType.bytes,
-          headers: await Utils.requestToken()
+          headers: await Utils.requestToken(),
         ),
       );
 
-      final directory = await getApplicationDocumentsDirectory();
-
-      final filePath =
-          '${directory.path}/relatorio_gastos.xlsx';
-
-      final file = File(filePath);
-
-      await file.writeAsBytes(response.data);
-
-      print('Arquivo salvo em: $filePath');
-
-      await OpenFilex.open(filePath);
+      ///Gerar File
+      await generateFile(response);
 
     } catch (e) {
       print(e);
     }
-}
-}
+  }
+
+  Future<void> generateFile(Response res) async {
+
+    AppPlatform platform = Utils.getCurrentPlatform();
+    /// ANDROID
+    if(platform == AppPlatform.android) {
+      final directory = Directory('/storage/emulated/0/Download');
+      if (!await directory.exists())  await directory.create(recursive: true);
+
+      final filePath = '${directory.path}/relatorio_gastos.xlsx';
+      final file = File(filePath);
+      await file.writeAsBytes(List<int>.from(res.data),);
+      print(filePath);
+      await OpenFilex.open(filePath);
+    }else
+    /// WEB
+    if(platform == AppPlatform.web){
+        final bytes = Uint8List.fromList(List<int>.from(res.data),);
+        final blob = html.Blob([bytes]);
+        final url = html.Url.createObjectUrlFromBlob(blob);
+        final anchor =
+        html.AnchorElement(href: url)
+          ..setAttribute(
+            "download",
+            "relatorio_gastos.xlsx",
+          )
+          ..click();
+        html.Url.revokeObjectUrl(url);
+      }
+    }
+  }
